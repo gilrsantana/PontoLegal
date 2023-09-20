738c53db8ed37daae923c39f93a4b9c72e3a3fd8
gilrsantana <g.r.santana@hotmail.com>
2023-09-20 06:09:30 -0300

    refactor: ICompanyService

--- PontoLegal.Service/Interfaces/ICompanyService.cs
+++ PontoLegal.Service/Interfaces/ICompanyService.cs
@@ -9,6 +9,7 @@ public interface ICompanyService
     Task<CompanyDTO?> GetCompanyByIdAsync(Guid id);
     Task<CompanyDTO?> GetCompanyByNameAsync(string modelName);
     Task<CompanyDTO?> GetCompanyByCnpjAsync(Cnpj cnpj);
+    Task<ICollection<CompanyDTO>> GetAllCompaniesAsync(int skip = 0, int take = 25);
     Task<bool> AddCompanyAsync(CompanyModel model);
     Task<bool> UpdateCompanyAsync(Guid id, CompanyModel model);
     Task<bool> RemoveCompanyByIdAsync(Guid id);
